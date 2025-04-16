f112ede3e7d57bdce6941954ee84e7a4e04dee01
Rakakaysan <165691974+Rakakaysan@users.noreply.github.com>
2025-04-16 14:17:02 +0700

    Add files via upload

---
+++ SQLQuery888.sql
@@ -0,0 +1,9 @@
+INSERT INTO [user] (
+ [nama],
+ [role],
+ [email],
+ [username],
+ [password]
+) VALUES
+ ('haris', 'admin', 'haris@gmail.com', 'haris', 'yahya'),
+ ('7G', 'siswa', 'user1@example.com', '7G', 'solid')
